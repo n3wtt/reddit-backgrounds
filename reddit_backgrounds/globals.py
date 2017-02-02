d586aca21425aa71e0650860aad318087ad0abd0
Kevin Haroldsen <kupiakos@gmail.com>
2017-02-02 00:24:25 -0700

    Change config filename

--- reddit_backgrounds/globals.py
+++ reddit_backgrounds/globals.py
@@ -1,6 +1,6 @@
 
 APP_ID = 'reddit-backgrounds'
 BACKGROUND_FILENAME = 'current-background'
-CONFIG_FILE = APP_ID + '.conf'
+CONFIG_FILE = 'settings.conf'
 CONFIG_SECTION = APP_ID
 USER_AGENT = APP_ID + ' by /u/kupiakos'
